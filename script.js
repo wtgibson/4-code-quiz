e23d56727a6a0b6fa6b8a5e48ecedc0c27982cd4
Gibbyswag <will.gibson7@gmail.com>
2020-03-24 16:00:21 -0700

    continued building quiz functions and added several eventlisteners on elements in JS and updated visibility in HTML and CSS

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ var answer0Element = document.querySelector("#answer0");
 var answer1Element = document.querySelector("#answer1");
 var answer2Element = document.querySelector("#answer2");
 var answer3Element = document.querySelector("#answer3");
+var startElement = document.querySelector("#start");
+var resultElement = document.querySelector("#result");
+var leaderboardLinkElement = document.querySelector("#leaderboard-link");
 
 var quizQuestions = [
     {question: "What country is Land Rover's parent company based?",
@@ -29,7 +32,7 @@ var quizQuestions = [
      answer: ["Horse", "Lion", "Bull", "Bird"],
      correct: "Bull",
     },
-]
+];
 
 var index = 0
 var secondsLeft = 100
@@ -42,7 +45,32 @@ function setTime() {
 
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
-            alert("Game Over")
+            alert("Times Up!")
         }
     }, 1000);
-}
+};
+
+startElement.addEventListener("click", function (event) {
+    event.preventDefault();
+    startElement.style.visibility = 'hidden';
+    questionElement.style.visibility = 'visible';
+    setTime();
+    quiz(event)
+    endQuiz();
+
+});
+
+answersElement.addEventListener("click", function (event) {
+    event.preventDefault();
+    resultElement.style.visibility = 'visible';
+    leaderboardLinkElement.style.visibility = 'hidden';
+});
+
+function quiz () {
+    console.log("Quiz Q&A # " + i)
+    questionElement.textContent = quizQuestions[i].question;
+    answer0Element.textContent = quizQuestions[i].answer[0];
+    answer1Element.textContent = quizQuestions[i].answer[1];
+    answer2Element.textContent = quizQuestions[i].answer[2];
+    answer3Element.textContent = quizQuestions[i].answer[3];
+};
